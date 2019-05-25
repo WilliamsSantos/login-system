e5262a7162343d82c6ce7cf4549a72795d791e34
WilliamsSantos <contato.williams.santos@gmail.com>
2019-05-25 19:24:23 -0300

    working on user_controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -5,14 +5,96 @@ const User = require('../models/User');
 
 // Get all cars
 exports.checkLoginType = async (req, reply) => {
-  const { name, email, cnpj, cpf, password } = checkLogin(req);
+  const { name, email, cpf_cnpj, password } = req.body; 
+  //  testes: '{"name":"Ryzzan", "email":"Williams@outlook.com","cpf_cnpj":34565434232, "password":4334345}'
+  const email_validate = emailValidates(email);
 
-  // {"name":"Ryzzan", "email":"Williams@outlook.com","cpf":2342, "cnpj":123231}
+  if(email_validate === true){
+    try { 
+      if (name && email && cpf_cnpj && password){
+        reply
+          .header('Content-Type', 'application/json; charset=utf-8')
+          .redirect('/user/checkLoginExistences')
+          .send({ 
+                "statusCode": 200, //# Resposta de protocolo
+                "result" : 'Form OK', //#retorna o valor esperado
+                "green" : 1,        //# 1 = sucess 0 = error  
+                "redCode" : 0,      //# Id da mensagem de erro
+                "message" : "All Ok.",// #mensagem vinculada ao login
+                "stackResult" : {
+                  "message": "OK" //#Retorna o objeto de retorno(informações do erro), lista de tratamento;
+                },
+          })
+        
+      }else{
+        if(!password){
+          reply
+            .header('Content-Type', 'application/json; charset=utf-8')
+            .send({ 
+                  "statusCode" : 404,
+                  "result" : 'Password is empty',
+                  "green" : 0,       
+                  "redCode" : 1,     
+                  "message" : "You need fill the password field.",
+                  "stackResult" : {
+                    "message" :`error: ${boom.boomify(err)}`
+                  }
+            });
+        }else{
+          reply
+          .header('Content-Type', 'application/json; charset=utf-8')
+          .send({ 
+                "statusCode" : 404,
+                "result" : 'Some input of Form is empty',
+                "green" : 0,       
+                "redCode" : 1,     
+                "message" : "You need Fill all the form.",
+                "stackResult" : {
+                  "message" :`error: ${boom.boomify(err)}`
+                }
+          });
+        }
+      }
+    } catch (err) {
+      throw boom.boomify(err)
+    }
+  }else{
+    reply
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .send({ 
+          "statusCode" : 404,
+          "result" : 'email is invalid',
+          "green" : 0,       
+          "redCode" : 1,     
+          "message" : "You need Fill all the form.",
+          "stackResult" : {
+            "message" :`error: ${boom.boomify(err)}`
+          }
+    });
+  }
+}
+
+exports.checkLoginExistences = async (req, reply) => {
+  const { email, password } = req.body; 
   
- try { 
-    if (checkLogin){
+  try{ 
+    User.find({ email, password }, (err, data) => {
+      if (err) {
+        reply
+          .header('Content-Type', 'application/json; charset=utf-8')
+          .send({
+          "statusCode" : 500, 
+            "result" : 'No user with the params!', 
+            "green" : 0,        
+            "redCode" : 5,      
+            "message" : "No data Found.",
+            "stackResult" : {
+              "message" : `Error: ${boom.boomify(err)}`
+            }
+          });
+      }
       reply
-      .header('Content-Type', 'application/json; charset=utf-8')
+        .header('Content-Type', 'application/json; charset=utf-8')
         .send({ 
               "statusCode": 200, //# Resposta de protocolo
               "result" : 'Form OK', //#retorna o valor esperado
@@ -23,53 +105,9 @@ exports.checkLoginType = async (req, reply) => {
                 "message": "OK" //#Retorna o objeto de retorno(informações do erro), lista de tratamento;
               },
         })
-          .redirect('/user/checkLoginExistences');
-    }else{
-      reply
-      .header('Content-Type', 'application/json; charset=utf-8')
-      .send({ 
-            "statusCode" : 404, //# Resposta de protocolo
-            "result" : 'Some input of Form is empty', //#retorna o valor esperado
-            "green" : 0,        //# 1 = sucess 0 = error  
-            "redCode" : 1,      //# Id da mensagem de erro
-            "message" : "You need Fill all the form.",// #mensagem vinculada ao login
-            "stackResult" : {
-              "message" :`error: ${boom.boomify(err)}`
-            }
-      });
-    }
-  } catch (err) {
-    throw boom.boomify(err)
-  }
-}
-
-exports.checkLoginExistences = async (req, reply) => {
-  const { name, email, cnpj, cpf, password } = checkLogin(req);
-
-  try{
-    const data = mongoose.find({  name:checkLogin.name, 
-                                  email: checkLogin.email,
-                                  cnpj: checkLogin.cnpj, 
-                                  cpf: checkLogin.cpf, 
-                                  password: checkLogin.password  
-                                }); 
-    
-    if(data){
-      reply
-      .header('Content-Type', 'application/json; charset=utf-8')
-      .send({
-        "statusCode" : 200, //# Resposta de protocolo
-          "result" : 'Some input of Form is empty', //#retorna o valor esperado
-          "green" : 1,        //# 1 = sucess 0 = error  
-          "redCode" : 3,      //# Id da mensagem de erro
-          "message" : "Login Sucess!.",// #mensagem vinculada ao login
-          "stackResult" : {
-            "message" : "User as loged-in"
-          }
-      }) 
-    }
+    });
   }catch(err){
-    throw boom.boomify(err)
+    throw boom.boomify(err);
   }
 }
 
@@ -138,3 +176,9 @@ exports.deleteCar = async (req, reply) => {
   }
 }
 */
+
+
+const emailValidates =(email) =>{
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
